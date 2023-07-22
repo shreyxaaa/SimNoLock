0923086033aec2ccd6b9e7bc3ec63bf7fe37d78c
shreyxaaa <139839974+shreyxaaa@users.noreply.github.com>
2023-07-22 18:53:21 +0530

    Update form.php

--- form.php
+++ form.php
@@ -4,48 +4,21 @@
     <title>User Details Form</title>
 </head>
 <body>
+    <h1>User Details Form</h1>
+    <form action="process_form.php" method="post">
+        <label for="firstName">First Name:</label>
+        <input type="text" name="firstName" id="firstName" required><br>
 
-<?php
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form data
-    $first_name = $_POST["first_name"];
-    $last_name = $_POST["last_name"];
-    $email = $_POST["email"];
-    $phone_number = $_POST["phone_number"];
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="lastName" id="lastName" required><br>
 
-    // Format the data as a string
-    $user_details = "First Name: " . $first_name . "\n" .
-                    "Last Name: " . $last_name . "\n" .
-                    "Email: " . $email . "\n" .
-                    "Phone Number: " . $phone_number . "\n";
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required><br>
 
-    // Open the file in append mode and save the user details
-    $file = fopen("user_details.txt", "a");
-    fwrite($file, $user_details);
-    fclose($file);
+        <label for="phone">Phone Number:</label>
+        <input type="tel" name="phone" id="phone" required><br>
 
-    echo "<h3>Thank you for submitting the form!</h3>";
-    echo "<p>Your details have been collected and saved.</p>";
-}
-?>
-
-<h2>User Details Form</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <label for="first_name">First Name:</label>
-    <input type="text" id="first_name" name="first_name" required><br><br>
-
-    <label for="last_name">Last Name:</label>
-    <input type="text" id="last_name" name="last_name" required><br><br>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required><br><br>
-
-    <label for="phone_number">Phone Number:</label>
-    <input type="text" id="phone_number" name="phone_number" required><br><br>
-
-    <input type="submit" value="Submit">
-</form>
-
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
